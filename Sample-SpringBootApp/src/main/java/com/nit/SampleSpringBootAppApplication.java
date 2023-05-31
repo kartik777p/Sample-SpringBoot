ddba7126155240766bf619c6eff5e5f32d438292
Neosoft <Neosoft@DESKTOP-AAIK2PJ>
2023-05-31 11:39:55 +0530

    Done

---
+++ Sample-SpringBootApp/src/main/java/com/nit/SampleSpringBootAppApplication.java
@@ -0,0 +1,13 @@
+package com.nit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SampleSpringBootAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SampleSpringBootAppApplication.class, args);
+	}
+
+}
